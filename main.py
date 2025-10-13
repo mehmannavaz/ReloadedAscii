3ffed45ba7794320655eed05f04ae40f9d8385de
mehmannavaz <grimirani@gmail.com>
2025-10-14 01:19:40 +0330

    feat+fix: change the docs format and add ascii class and calculators

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ Example1 = """
 |_|
 """
 
-Example2 = """
- ______ 
-| _  _ |
-||_||_||
-|______|
+Example2 = """    
+ __________  
+|  __  __  |
+| |  ||  | |
+| |__||__| |
+|          | 
+|__________|
 """
 
 Example3 = """
@@ -21,6 +23,20 @@ Example3 = """
   |_|_        _|_|
     |_|      |_|
 """
+
+class ascii:
+    def __init__(self, art):
+        self.art = art
+        self.length = art.count('\n')
+        self.width = max([len(line) for line in art.split('\n')])
+
+        # Turned
+        # https://github.com/mehmannavaz/ReloadedAscii/blob/main/docs/01-Theory/02-HowToTurn
+        self.turned_length = self.length + 1 
+        self.turned_width = self.width + 5
+
+
+def calculateTurnedWidth:
 
 
 def _in_box:
